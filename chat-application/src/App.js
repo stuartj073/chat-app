e05beb55f265cf99891c7914d0203b4c5732254e
stuartj073 <stuartj073@gmail.com>
2022-08-04 08:26:58 +0000

    Add App function to app.js

--- chat-application/src/App.js
+++ chat-application/src/App.js
@@ -1,3 +1,17 @@
 import { ChatEngine } from 'react-chat-engine';
 
 import './App.css';
+
+
+const App = () => {
+    return (
+        <ChatEngine 
+            height="100vh"
+            projectID=""
+            userName=""
+            userSecret=""           
+        />
+    )
+}
+
+export default App;
